691fda847747e9a992a16e3e07538b403efb096e
Pastilhas <joao.campos1221@gmail.com>
2019-11-24 13:36:07 +0000

    Started download app

--- ftp/clientTCP.c
+++ ftp/clientTCP.c
@@ -15,18 +15,12 @@
 #define SERVER_ADDR "192.168.28.96"
 
 int main() {
-	//ANCHOR open socket
-  int network_socket;
-  network_socket = open_socket(AF_INET, SOCK_STREAM, 0);
+	// open socket
+  int network_socket = open_socket(AF_INET, SOCK_STREAM, 0);
 
-	//ANCHOR create socket address
-  struct sockaddr_in server_addr;
-  bzero((char*)&server_addr, sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = htons(SERVER_PORT);            /*server TCP port must be network byte ordered */
-  server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR); /*32 bit Internet address network byte ordered*/
+	// create socket address
 
-  //ANCHOR connect to socket
+  // connect to socket
   connect_socket(network_socket, (struct sockaddr*) &server_addr, sizeof(server_addr);
 
   close(network_socket);
@@ -40,6 +34,16 @@ int open_socket(int domain, int type, int protocol){
     exit(1);
   }
   return ret;
+}
+
+struct sockaddr_in get_address(){
+  struct sockaddr_in server_addr;
+  bzero((char*)&server_addr, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(SERVER_PORT);            /*server TCP port must be network byte ordered */
+  server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR); /*32 bit Internet address network byte ordered*/
+
+  return server_addr;
 }
 
 int connect_socket(int sockfd, const struct sockaddr *addr, socklen_t addrlen){
